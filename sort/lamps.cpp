28f6f4bd4af73889dd7ce676fe50980d632c30aa
Ruslan Tikhonov <tihoo99@icloud.com>
2020-03-19 16:08:34 +1000

    Update README.md

---
+++ sort/lamps.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int N;
+    cin >> N;
+    int array[2*N] = {0};
+    int max = 0;
+    for (int i = 0; i < 2*N; i++)
+    {
+        cin >> array[i];
+        
+    }
+    {
+}
