9474e6d97116f76195715230be9ead160823db13
Ruslan Tikhonov <tihoo99@icloud.com>
2020-04-13 11:44:03 +1000

    Update heap class

--- heaps/my_heap_realization/heap_class.cpp
+++ heaps/my_heap_realization/heap_class.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <iostream>
 #include <fstream>
+#include <iostream>
 
 using namespace std;
 
@@ -40,9 +41,9 @@ public:
 			leftChild = 2 * index + 1;
 			rightChild = 2 * index + 2;
 
-			auto new_ind = (leftChild<heapSize() && heap[leftChild] > heap[rightChild]) ? leftChild : rightChild;
+			auto new_ind = (leftChild < heapSize() && heap[leftChild] > heap[rightChild]) ? leftChild : rightChild;
 
-			if (new_ind < heapSize() && heap[index]<heap[new_ind])
+			if (new_ind < heapSize() && heap[index] < heap[new_ind])
 			{
 				swap(heap[index], heap[new_ind]);
 				index = new_ind;
@@ -54,7 +55,6 @@ public:
 
 		} while (working);
 	}
-
 
 	void buildHeap(int arr[], int n)
 	{
@@ -69,7 +69,7 @@ public:
 	{
 		int result = heap[0];
 		heap[0] = heap[heapSize() - 1];
-		heap.erase(heap.end() - 1);
+		heap.pop_back();
 		heapify(0);
 		//cout << result;
 		return result;
@@ -77,32 +77,131 @@ public:
 
 	int getElement(int i)
 	{
-		return(heap[i]);
+		return (heap[i]);
 	}
 };
 
-bheap heap;
+bheap maxheap;
 
-int main() {
+class min_bheap
+{
+	vector<int> heap;
+
+public:
+	int heapSize()
+	{
+		return (heap.size());
+	}
+
+	void add(int value)
+	{
+		heap.push_back(value);
+		int i = heapSize() - 1;
+		int parent = (i - 1) / 2;
+
+		while (i > 0 && heap[parent] > heap[i])
+		{
+			swap(heap[i], heap[parent]);
+			i = parent;
+			parent = (i - 1) / 2;
+		}
+	}
+
+	void heapify(int index)
+	{
+		int leftChild;
+		int rightChild;
+		auto working = true;
+
+		do
+		{
+			leftChild = 2 * index + 1;
+			rightChild = 2 * index + 2;
+
+			auto new_ind = (leftChild < heapSize() && heap[leftChild] < heap[rightChild]) ? leftChild : rightChild;
+
+			if (new_ind < heapSize() && heap[index] > heap[new_ind])
+			{
+				swap(heap[index], heap[new_ind]);
+				index = new_ind;
+			}
+			else
+			{
+				working = false;
+			}
+
+		} while (working);
+	}
+
+	void buildHeap(int arr[], int n)
+	{
+		vector<int> heap(arr, arr + n);
+		for (int i = heapSize() / 2; i >= 0; i--)
+		{
+			heapify(i);
+		}
+	}
+
+	int pickMin()
+	{
+		int result = heap[0];
+		heap[0] = heap[heapSize() - 1];
+		heap.erase(heap.begin() + heapSize() - 1);
+		heapify(0);
+		return result;
+	}
+
+	int getElement(int i)
+	{
+		return (heap[i]);
+	}
+};
+
+min_bheap minheap;
+
+int main()
+{
 	ifstream fin("input.txt");
-    ofstream fout("output.txt");
-    long long int N, M;
-    fin >> N >> M;
-    for (long long int i = 0; i < N; i++) {
-        long long k;
-        fin >> k;
-        heap.add(k);
-    }
+	ofstream fout("output.txt");
 
-    for (int i =0; i< M; i++) {
-        long long max_thirst = heap.pickMax();
-        max_thirst /= 10;
-        heap.add(max_thirst);
-    }
+	int n;
+	fin >> n;
+	int tmp;
+	vector<int> sort_array;
 
-    long long sum = 0;
-    for (int i = 0; i < N; i++) {
-        sum += heap.getElement(i);
-    }
-    fout << sum;
+	for (int index = 0; index < n; index++)
+	{
+		fin >> tmp;
+		if (index == 0)
+			maxheap.add(tmp);
+		else
+		{
+			if (index % 2 != 0)
+			{
+				if (tmp < maxheap.getElement(0))
+				{
+					minheap.add(maxheap.pickMax());
+					maxheap.add(tmp);
+				}
+				else
+					minheap.add(tmp);
+			}
+			if (index % 2 == 0)
+			{
+				if (tmp > minheap.getElement(0))
+				{
+					maxheap.add(minheap.getElement(0));
+					minheap.add(tmp);
+				}
+				else
+					maxheap.add(tmp);
+			}
+		}
+		sort_array.push_back(maxheap.getElement(0));
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		fout << sort_array[i] << " ";
+	}
 }
