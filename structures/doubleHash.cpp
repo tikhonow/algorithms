f336256132153b330209b3f7c78c628d2dfbe30f
Ruslan Tikhonov <tihoo99@icloud.com>
2020-06-14 21:10:45 +1000

    add: hash table class

--- structures/doubleHash.cpp
+++ structures/doubleHash.cpp
@@ -1,81 +1,101 @@
-#include <iostream>
-#include <stdlib.h>
-#include <time.h>
 #include <vector>
 #include <algorithm>
 #include <fstream>
 
-using namespace std;
-
-int m = 1000000;
-std::vector<int> DoubleHashing(m, -1);
-
 std::ifstream fin("input.txt");
 std::ofstream fout("output.txt");
 
-void Delete(int x)
+class doublehash
 {
-    int hash = (x % m);
-    // Double Hashing - Insert
-    int hash2 = 1 + (x % (m - 1)); // 1 + (k mod (m - 1))
-    int k = 0;
-    int hashDouble = (hash + k * hash2) % m;
-    while (DoubleHashing[hashDouble] != -1 && DoubleHashing[hashDouble] != x && k < m)
+    int hashsize;
+    std::vector<int> values;
+
+private:
+
+    int hash11(int x)
     {
-        k++;
-        hashDouble = (hash + k * hash2) % m;
+        return(x % hashsize);
     }
-    DoubleHashing[hashDouble] = -2;
 
-    return;
-}
-
-void Insert(int x)
-{
-    int hash = (x % m);
-    if (DoubleHashing.at(hash) == x)
+    int hash22(int x)
     {
+        return(1 + (x % (hashsize - 1)));
+    }
+
+public:
+
+    doublehash()
+    {
+        hashsize = 1000000;
+        for (int k = 0; k < hashsize; k++)
+        {
+            values.push_back(-1);
+        }
+
+    }
+
+    void Delete(int x)
+    {
+        int hash = hash11(x);
+
+        int hash2 = hash22(x);
+        int k = 0;
+        int hashDouble = (hash + k * hash2) % hashsize;
+        while (values[hashDouble] != -1 && values[hashDouble] != x && k < hashsize)
+        {
+            k++;
+            hashDouble = (hash + k * hash2) % hashsize;
+        }
+        values[hashDouble] = -2;
+
         return;
     }
-    // Double Hashing - Insert
-    int hash2 = 1 + (x % (m - 1)); // 1 + (k mod (m - 1))
-    int k = 0;
-    int hashDouble = (hash + k * hash2) % m;
-    while (DoubleHashing[hashDouble] != -1 && DoubleHashing[hashDouble] != -2 && k < m)
-    {
-        k++;
-        hashDouble = (hash + k * hash2) % m;
-    }
-    DoubleHashing[hashDouble] = x;
 
-    return;
-}
+    void Insert(int x)
+    {
+        int hash = hash11(x);
+        if (values.at(hash) == x)
+        {
+            return;
+        }
+
+        int hash2 = hash22(x); 
+        int k = 0;
+        int hashDouble = (hash + k * hash2) % hashsize;
+        while (values[hashDouble] != -1 && values[hashDouble] != -2 && k < hashsize)
+        {
+            k++;
+            hashDouble = (hash + k * hash2) % hashsize;
+        }
+        values[hashDouble] = x;
+
+        return;
+    }
+
+    void output()
+    {
+        std::sort(values.begin(), values.end());
+        for (int i = 0; i < hashsize; i++)
+        {
+            if (values[i] > 0)
+            {
+                fout << values[i] << "  ";
+            }
+        }
+    }
+};
 
 int main()
 {
-    int size = 0, LinCol = 0, QuadCol = 0, DoubCol = 0;
+    doublehash table;
     int a = 1;
     while (a != 0)
     {
         fin >> a;
-        if (a > 0)
-        {
-            Insert(a);
-            size++;
-        }
-        else
-        {
-            Delete(-a);
-            size--;
-        }
+        a > 0 ? table.Insert(a) : table.Delete(-a);
     }
-
-    std::sort(DoubleHashing.begin(), DoubleHashing.end());
-    for (int i = 0; i < m; i++)
-    {
-        if (DoubleHashing[i] > 0)
-        {
-            fout << DoubleHashing[i] << "  ";
-        }
-    }
+    table.output();
+    fin.close();
+    fout.close();
+    return 0;
 }
