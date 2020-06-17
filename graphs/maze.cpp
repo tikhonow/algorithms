833c45ea6350c9f4b6ddbe4486cd89bb46ad8f74
Ruslan Tikhonov <tihoo99@icloud.com>
2020-06-17 14:11:21 +1000

    some changes

--- graphs/maze.cpp
+++ graphs/maze.cpp
@@ -1,92 +1,61 @@
 #include <fstream>
+#include <vector>
 
-using namespace std;
-const int MAX = 100;
+const int MAXSIZE = 100;
 const int WALL = -1;
 const int BLANK = -2; 
-int N;
 
-int px[W * H], py[W * H];
-int len;
-int grid[H][W];
-
-int  lee(int ax, int ay, int bx, int by)
+int lee(std::vector<int> maze[MAXSIZE], int ax, int ay, int bx, int by, int N)
 {
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
-    int d, x, y, k;
+    int wave = 0;
     bool stop;
 
-    d = 0;
-    grid[ay][ax] = 0;
+    maze[ay][ax] = 0;
+    
     do
     {
         stop = true; 
-        for (y = 0; y < N; ++y)
-            for (x = 0; x < N; ++x)
-                if (grid[y][x] == d) 
+        for (auto y = 0; y < N; ++y)
+            for (auto x = 0; x < N; ++x)
+                if (maze[y][x] == wave) 
                 {
-                    for (k = 0; k < 4; ++k) 
+                    for (auto k = 0; k < 4; ++k) 
                     {
                         int iy = y + dy[k], ix = x + dx[k];
                         if (iy >= 0 && iy < N && ix >= 0 && ix < N &&
-                            grid[iy][ix] == BLANK)
+                            maze[iy][ix] == BLANK)
                         {
                             stop = false;       
-                            grid[iy][ix] = d + 1; 
+                            maze[iy][ix] = wave + 1; 
                         }
                     }
                 }
-        d++;
-    } while (!stop && grid[by][bx] == BLANK);
+        wave++;
+    } while (!stop && maze[by][bx] == BLANK);
 
-    len = grid[by][bx]; 
-    return len;
-    x = bx;
-    y = by;
-    d = len;
-    while (d > 0)
-    {
-        px[d] = x;
-        py[d] = y; 
-        d--;
-        for (k = 0; k < 4; ++k)
-        {
-            int iy = y + dy[k], ix = x + dx[k];
-            if (iy >= 0 && iy < H && ix >= 0 && ix < W &&
-                grid[iy][ix] == d)
-            {
-                x = x + dx[k];
-                y = y + dy[k]; 
-                break;
-            }
-        }
-    }
-    px[0] = ax;
-    py[0] = ay; 
+    return maze[by][bx];
 }
 
 int main()
 {
-    int x , y, x1 ,y1;
-    ifstream fin("input.txt");
-    ofstream fout("output.txt");
+    int N, x , y, x1 ,y1;
+    char cell;
+    std::ifstream fin("input.txt");
+    std::ofstream fout("output.txt");
+    std::vector<int> maze[MAXSIZE];
     fin >> N >> x >> y >> x1 >> y1;
-    for (int i = 0; i < 1;i++)
-    {
-        x--;
-        y--;
-        x1--;
-        y1--;
-    }
+    x--, y--, x1--, y1--;
+
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
-            char a;
-            fin >> a;
-            grid[i][j] = (a == '#' ? WALL : BLANK); //ставим стены
+            fin >> cell;
+            int a = (cell == '#' ? WALL : BLANK);
+            maze[i].push_back(a);
         }
     }
-    fout << lee(x, y, x1, y1);
+    fout << lee(maze,x, y, x1, y1, N);
 }
